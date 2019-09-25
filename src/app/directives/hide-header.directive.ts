b4d3c00f044cba2f9b525ae8725e273c5b37b09e
felipe <felipe.dultra@etec.sp.gov.br>
2019-09-25 16:07:07 -0300

    Primeiro commit

---
+++ src/app/directives/hide-header.directive.ts
@@ -0,0 +1,30 @@
+import { Directive, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appHideHeader]',
+  host: {
+    '(ionScroll)': 'onContentScroll($event)'
+  }
+})
+export class HideHeaderDirective {
+  @Input('header') header: any;
+  private lastY: number = 0;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    this.header = this.header.el;
+
+    this.renderer.setStyle(this.header, 'transition', 'margin-top 400ms');
+  }
+
+  onContentScroll(event: any) {
+    if (event.detail.scrollTop > this.lastY) {
+      this.renderer.setStyle(this.header, 'margin-top', `-${this.header.clientHeight}px`);
+    } else {
+      this.renderer.setStyle(this.header, 'margin-top', '0px');
+    }
+
+    this.lastY = event.detail.scrollTop
+  }
+}
---
+++ src/app/list/list.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+
+import { ListPage } from './list.page';
+import { HideHeaderDirective } from '../directives/hide-header.directive';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: ListPage
+      }
+    ])
+  ],
+  declarations: [ListPage, HideHeaderDirective]
+})
+export class ListPageModule {}
