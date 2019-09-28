8bf26c0671c414c61e074d6e25e9b3e5b71ee6e3
felipe <felipe.dultra@etec.sp.gov.br>
2019-09-28 20:04:17 -0300

    Alteração nas imagens dos produtos e na página de 'descrição'.

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -8,17 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ListPage implements OnInit {
 
   public listaCardapio = [
-    { nome: 'Sushi', preco: 22.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Sashimi', preco:  30.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Hossomakis', preco:  17.50, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Yakisoba', preco:  15.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Robata', preco:  20.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Uramakis', preco:  9.99, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Niguiri', preco:  10.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Tempura', preco:  30.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Wasabi', preco:  23.00, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Temakis', preco:  8.80, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' },
-    { nome: 'Ramen', preco:  8.80, descricao: '', img: 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y' }
+    { nome: 'Sushi', preco: 22.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQYUN5eVrPefcHyHd0r_cOWED5MT5wHtK7_0eHtHjFzOjQyLoW4' },
+    { nome: 'Sashimi', preco:  30.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRHtMr6dWrjqW1867_bQf5Njh52QSESiB2S5jJZQTzw1UQioVG3' },
+    { nome: 'Hossomakis', preco:  17.50, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSiUdIdoLQ6HbK0AFyoSrVSmbbvp7l7Mpf6mu9pBuhllNy7KrJ5' },
+    { nome: 'Yakisoba', preco:  15.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRrP__9MI7fbs8jWqOtWA4Yn3r0EPHxnfzAru3XUx6-M3xRXD7R' },
+    { nome: 'Robata', preco:  20.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQhn_c4PN5Q7846pjpG0pzZiI5ixzuoUbVWxeRGx_pAwcXm2bTU' },
+    { nome: 'Uramakis', preco:  9.99, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSrAAD3kKGkIhNTs6UH8tN1dBAXtEfT2ys9iE8ibo37bac_qqy3' },
+    { nome: 'Niguiri', preco:  10.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmqPdnThCXy45m3bz2avKGjZs-bT7jKBVIdMhNiToeDFrsea43' },
+    { nome: 'Tempura', preco:  30.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ5hnJVqFt4LxD6cZGKrHYiS6ygCwigds4sdY1c29oNmqgQ1AVQ' },
+    { nome: 'Wasabi', preco:  23.00, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRnSjNIYJw_C6lOkCW1qkp0Hr098fz43MW46ikfQu5Ifjx4x9-' },
+    { nome: 'Temakis', preco:  8.80, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXscIr91pQ0j5S6AIGxWUDxokvc2Jsg2kd0rnVOr3obTtAR9Nc' },
+    { nome: 'Ramen', preco:  8.80, descricao: '', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ9m29k2xsO2VciFKm0bWhhK1al5uDi1tlZt5dmtDPcwCXTAXEn' }
 
   ];
 
