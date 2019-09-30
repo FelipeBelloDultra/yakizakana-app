03baf6a797f581f4842303d56b071f415c44306a
felipe <felipe.dultra@etec.sp.gov.br>
2019-09-29 22:04:52 -0300

    Pequenas alterações (incluindo nova cor)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HideHeaderDirective } from '../directives/hide-header.directive';
 
-
-
 @NgModule({
   declarations: [HideHeaderDirective],
   exports: [HideHeaderDirective],
