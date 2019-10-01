3ca12ba827b7dfe6bd44eac852b2e4273f1659bc
Etec Ibitinga <e161dir@cps.sp.gov.br>
2019-10-01 20:38:49 -0300

    alteração na tela de login e criação da página de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -16,7 +16,9 @@ const routes: Routes = [
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
   { path: 'about', loadChildren: './about/about.module#AboutPageModule' },
-  { path: 'description', loadChildren: './description/description.module#DescriptionPageModule' },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'description', loadChildren: './description/description.module#DescriptionPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
+
 
 ];
 
