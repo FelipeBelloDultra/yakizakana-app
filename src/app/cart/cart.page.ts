a7ee587373e505163973b946613d3c25491e87f8
Etec Ibitinga <e161dir@cps.sp.gov.br>
2019-10-11 20:41:39 -0300

    Colocando soma dos produtos na tela 'Cart'.

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -9,13 +9,21 @@ export class CartPage implements OnInit {
 
   constructor() { }
 
-  public cart = [
+  public cartList = [
     {nome: 'test0', preco: 20, img: 'https://www.gravatar.com/avatar/24?d=monsterid&f=y'},
     {nome: 'test1', preco: 22, img: 'https://www.gravatar.com/avatar/24?d=monsterid&f=y'},
     {nome: 'test2', preco: 17, img: 'https://www.gravatar.com/avatar/24?d=monsterid&f=y'}
   ]
 
+  public itens: number = 0;
+  public amount: number = 0;
+
   ngOnInit() {
+    for (let prod of this.cartList) {
+      this.amount = this.amount + prod.preco;
+      this.itens++;
+    }
+  
   }
 
 }
